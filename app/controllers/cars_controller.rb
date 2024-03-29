730795cf766cb45e01f499e24ce39d10620de0a5
Herbert M. <hmorales5@tutanota.com>
2024-03-29 18:56:30 -0500

    cars controller allows for 3 route access

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,32 @@
 class CarsController < ApplicationController
+  before_action :set_car, only: [:show]
+  # GET /cars
   def index
     cars = Car.all 
     render json: cars
+  end
+
+  # GET /cars/1
+  def show
+    render json: @car
+  end
+
+  # POST /cars
+  def create 
+    @car = Car.new(car_params)
+    
+    if @car.save
+      render json: @car, status: :created
+    else
+      render json: @car.errors, status: :unprocessable_entity
+    end
+  end
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  def car_params
+    params.require(:car).permit(:id, :make, :model, :year)
   end
 end
